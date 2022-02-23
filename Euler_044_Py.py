092342d6d7e5d46cad423ccc6b466470aeb83ceb
Ariel Tynan <tynan.a@northeastern.edu>
2022-02-23 13:10:20 -0500

    Not working yet

--- Euler_044_Py.py
+++ Euler_044_Py.py
@@ -6,29 +6,31 @@
 # For n from 1 to high number, check if
     # 3n + 1 in pentnum and 3n^2 + 2n + 1 in pentnum
 
+import math
+
+
 #Create array of pentagonal numbers
 pentNums = [] #array of pentagonal numbers
-gen = 10000 #arbitrary range of how many pentnums generated
-Dmin = 100000
+gen = 1000000 #arbitrary range of how many pentnums generated
+Dmin = 10000
 
 for i in range(0,gen): # number of pentnums generated
     num = int((i+1)*(3*(i+1) - 1)/2)
     pentNums.extend([num]) #dynamically growing array
-    #print(pentnums[i])
+    #print(pentNums[i])
 
-for a in range(1,len(pentNums)): #range of values n tested
-    for b in range(a + 1,len(pentNums)):
-        if (pentNums[a] + pentNums[b])/2 in pentNums and (pentNums[a] - pentNums[b])/2 in pentNums and pentNums[a] < Dmin: # a is dif, b is sum of two pent nums
-            Dmin = pentNums[a]
-            print(pentNums[a], pentNums[b])
+for a in range(1,gen): #range of values n tested
+    for b in range(1,gen):
+        valdif = 1/6 + math.sqrt(abs(2/3*(abs(6*a*b + 3*b*b - b)/2)+1/36))
+        valsum = 1/6 + math.sqrt(abs(2/3*(abs(6*a*b + 3*b*b - b + 6*a*a - 2*a)/2)+1/36))
+        #print(val)
+        if abs(2/3*(6*a*b + 3*b*b - b)/2) > Dmin:
+            break
+        else:
+            if valdif.is_integer() and valsum.is_integer() and abs(6*a*b + 3*b*b - b)/2 < Dmin:
+                #Dmin = abs(pentNums[a+1]-pentNums[b+1])
+                print(a, (3*a*a - a)/2, b, (3*b*b - b)/2)
+        
+        
         
 print("Answer: ", Dmin)
-
-
-
-
-
-
-# Design
-# Create array of pentagonal numbers
-# For each new pent num, check sum and difference of each and every other pent nums in list to confirm if pent
